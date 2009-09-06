7c9a46e4e2d1c4d31801daa5023e34f3365b09f2
Zhang Huangbin <Zhang Huangbin@localhost>
2009-09-06 23:33:24 +0800

    Doc cleanup.

--- docs/samples/iredadmin.sql
+++ docs/samples/iredadmin.sql
@@ -1,7 +1,3 @@
-# CREATE DATABASE iredadmin DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-# GRANT INSERT,UPDATE,DELETE,SELECT on iredadmin.* to iredadmin@localhost identified by 'secret_passwd';
-#USE iredadmin;
-
 #
 # Session table required by webpy session module.
 #
@@ -10,45 +6,4 @@ CREATE TABLE sessions (
     atime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     data TEXT,
     INDEX session_id_index (session_id)
-) TYPE=MyISAM;
-
-#
-# Store all operations.
-#
-CREATE TABLE log (
-    timestamp DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
-    admin VARCHAR(255) NOT NULL DEFAULT '',
-    domain VARCHAR(255) NOT NULL DEFAULT '',
-    action VARCHAR(255) NOT NULL DEFAULT '',
-    data VARCHAR(255) NOT NULL DEFAULT '',
-    KEY TIMESTAMP (TIMESTAMP),
-    INDEX timestamp_index (timestamp),
-    INDEX admin_index (admin),
-    INDEX domain_index (domain),
-    INDEX action_index (action)
-) TYPE=MyISAM;
-
-#
-# author: Who public this announcement.
-# admins: Who will see this announcement. Default is ALL admins.
-#         Multiple admins should be seperated by comma.
-# starttime: When this ann msg will be displayed. Default is NOW.
-# endtime:   When it should not be displayed.
-# subject: message subject.
-# message: content of ann msg.
-#
-CREATE TABLE announcements (
-    id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-    author VARCHAR(250) NOT NULL,
-    admins VARCHAR(250) NOT NULL DEFAULT 'ALL',
-    starttime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 
-    endtime TIMESTAMP,
-    subject VARCHAR(250) NOT NULL,
-    message text NOT NULL,
-    PRIMARY KEY (id),
-    INDEX id_index (id),
-    INDEX author_index (author),
-    INDEX admins_index (admins),
-    INDEX starttime_index (starttime),
-    INDEX endtime_index (endtime)
 ) TYPE=MyISAM;
