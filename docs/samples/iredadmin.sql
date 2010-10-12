3ab713abb0f305b98fbb7583d85c5f055f153099
Zhang Huangbin <Zhang Huangbin@localhost>
2010-10-12 21:38:30 +0800

    Sync.

--- docs/samples/iredadmin.sql
+++ docs/samples/iredadmin.sql
@@ -5,9 +5,38 @@
 #
 # Session table required by webpy session module.
 #
-CREATE TABLE sessions (
-    session_id CHAR(128) UNIQUE NOT NULL,
-    atime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    data TEXT,
-    INDEX session_id_index (session_id)
+CREATE TABLE IF NOT EXISTS `sessions` (
+    `session_id` CHAR(128) UNIQUE NOT NULL,
+    `atime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `data` TEXT
+) ENGINE=MyISAM;
+
+#
+# Store all admin operations.
+#
+CREATE TABLE IF NOT EXISTS `log` (
+    `id` BIGINT(20) UNSIGNED AUTO_INCREMENT,
+    `timestamp` TIMESTAMP(14) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `admin` VARCHAR(255) NOT NULL,
+    `ip` VARCHAR(15) NOT NULL,
+    `domain` VARCHAR(255) NOT NULL DEFAULT '',
+    `username` VARCHAR(255) NOT NULL DEFAULT '',
+    `event` VARCHAR(10) NOT NULL DEFAULT '',
+    `loglevel` VARCHAR(10) NOT NULL DEFAULT 'info',
+    `msg` VARCHAR(255) NOT NULL,
+    KEY id (id),
+    INDEX (timestamp),
+    INDEX (admin),
+    INDEX (ip),
+    INDEX (domain),
+    INDEX (username),
+    INDEX (event),
+    INDEX (loglevel)
+) ENGINE=MyISAM;
+
+CREATE TABLE IF NOT EXISTS `updatelog` (
+    `id` BIGINT(20) UNSIGNED AUTO_INCREMENT,
+    `date` DATE NOT NULL,
+    KEY id (id),
+    INDEX (date)
 ) ENGINE=MyISAM;
