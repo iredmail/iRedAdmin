380c187bda4fea84373f346a28cc38de36564c59
Zhang Huangbin <Zhang Huangbin@localhost>
2012-06-09 15:05:06 +0800

    Fix typo error: __version_ose -> __version_ose__.

--- libs/__init__.py
+++ libs/__init__.py
@@ -1,6 +1,6 @@
 __author__ = 'Zhang Huangbin'
 __author_mail__ = 'zhb@iredmail.org'
-__version_ose = '0.1.8'
+__version_ose__ = '0.1.8'
 __url_latest_ose__ = 'http://iredmail.org/version/check.py/ose.xml'
 __no__ = ''
 __id__ = ''
