43cedde26994fc89a7d34111b7468568400dcfa0
Zhang Huangbin <Zhang Huangbin@localhost>
2018-12-17 13:14:00 +0800

    Cron job should exist if amavisd is enabled.

--- tools/cleanup_amavisd_db.py
+++ tools/cleanup_amavisd_db.py
@@ -30,6 +30,7 @@
 
 import os
 import sys
+import time
 import web
 
 os.environ['LC_ALL'] = 'C'
@@ -47,18 +48,33 @@ backend = settings.backend
 logger.info('Backend: %s' % backend)
 logger.info('SQL server: %s:%d' % (settings.amavisd_db_host, int(settings.amavisd_db_port)))
 
-query_size_limit = 100
+if (not settings.amavisd_enable_logging) or (not settings.amavisd_enable_quarantine):
+    sys.exit("Amavisd is not enabled. SKIP.")
+
+query_size_limit = settings.AMAVISD_CLEANUP_QUERY_SIZE_LIMIT
 keep_quar_days = settings.AMAVISD_REMOVE_QUARANTINED_IN_DAYS
 keep_inout_days = settings.AMAVISD_REMOVE_MAILLOG_IN_DAYS
 
 conn = ira_tool_lib.get_db_conn('amavisd')
 
+if settings.backend in ['mysql', 'ldap']:
+    # Querying (SELECT) without locking. Require MySQL 5.0+ and InnoDB.
+    #
+    # Since we're dealing with sql records created days ago, no new records
+    # will be inserted with that date, it's safe to use dirty read.
+    logger.info('Enable dirty read for querying without locking SQL tables.')
+    try:
+        conn.query('SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED')
+    except Exception, e:
+        logger.error('Cannot enable dirty read: %s' % repr(e))
+
+
 # Removing records from single table.
 def remove_from_one_table(conn, sql_table, index_column, removed_values):
     total = len(removed_values)
 
-    # Delete 1000 records each time
-    offset = 1000
+    # Delete how many records each time
+    offset = query_size_limit
 
     if total:
         loop_times = total / offset
@@ -66,8 +82,9 @@ def remove_from_one_table(conn, sql_table, index_column, removed_values):
             loop_times += 1
 
         for i in range(loop_times):
-            removing_values = removed_values[offset*i: offset*(i+1)]
-            logger.info('\t[-] Deleting records from table `%s`: %d - %d' % (sql_table, i*offset, i*offset + len(removing_values)))
+            removing_values = removed_values[offset * i: offset * (i + 1)]
+            logger.info(
+                '\t[-] Deleting records: %d - %d (%s)' % (i * offset, i * offset + len(removing_values), time.ctime()))
             conn.delete(sql_table,
                         vars={'ids': removing_values},
                         where='%s IN $ids' % index_column)
@@ -83,20 +100,22 @@ if ira_tool_lib.sql_dbn == 'mysql':
 elif ira_tool_lib.sql_dbn == 'postgres':
     sql_where = """quar_type = 'Q'
                    AND time_iso < CURRENT_TIMESTAMP - INTERVAL '%d DAYS'""" % keep_quar_days
+else:
+    logger.error('Invalid SQL backend: %s' % ira_tool_lib.sql_dbn)
+    sys.exit()
 
 counter_msgs = 0
 while True:
-
     qr = conn.select('msgs',
                      what='mail_id',
                      where=sql_where,
                      limit=query_size_limit)
 
     if qr:
-        ids = [id.mail_id for id in qr]
+        ids = [r.mail_id for r in qr]
         _total = len(ids)
 
-        logger.info('\t[-] Deleting records: %d - %d' % (counter_msgs+1, counter_msgs + _total))
+        logger.info('\t[-] Deleting records: %d - %d (%s)' % (counter_msgs + 1, counter_msgs + _total, time.ctime()))
 
         conn.delete('msgs', vars={'ids': ids}, where='mail_id IN $ids')
         conn.delete('msgrcpt', vars={'ids': ids}, where='mail_id IN $ids')
@@ -114,6 +133,11 @@ elif ira_tool_lib.sql_dbn == 'postgres':
     sql_where = """quar_type <> 'Q'
                    AND time_iso < CURRENT_TIMESTAMP - INTERVAL '%d DAYS'""" % keep_inout_days
 
+# We experienced an issue with PostgreSQL, it always return an non-existing
+# SQL record, and it causes endless loop. As a hack, we store all removed
+# `mail_id` and compare new `mail_id` with this list.
+_removed_ids = set()
+
 counter_msgrcpt = 0
 while True:
     qr = conn.select('msgs',
@@ -122,15 +146,21 @@ while True:
                      limit=query_size_limit)
 
     if qr:
-        ids = [id.mail_id for id in qr]
+        ids = [str(r.mail_id) for r in qr]
         _total = len(ids)
 
-        logger.info('\t[-] Deleting records: %d - %d' % (counter_msgrcpt+1, counter_msgrcpt + _total))
+        _removing_ids = list(set(ids) - set(_removed_ids))
+        if not _removing_ids:
+            break
 
-        conn.delete('msgs', vars={'ids': ids}, where='mail_id IN $ids')
-        conn.delete('msgrcpt', vars={'ids': ids}, where='mail_id IN $ids')
+        logger.info(
+            '\t[-] Deleting records: %d - %d (%s)' % (counter_msgrcpt + 1, counter_msgrcpt + _total, time.ctime()))
+
+        conn.delete('msgs', vars={'ids': _removing_ids}, where='mail_id IN $ids')
+        conn.delete('msgrcpt', vars={'ids': _removing_ids}, where='mail_id IN $ids')
 
         counter_msgrcpt += _total
+        _removed_ids.update(ids)
     else:
         break
 
@@ -157,7 +187,7 @@ qr = conn.select('quarantine', what='mail_id')
 for i in qr:
     quar_mail_ids.add(i.mail_id)
 
-invalid_quar_mail_ids = [id for id in quar_mail_ids if id not in msgs_mail_ids]
+invalid_quar_mail_ids = [i for i in quar_mail_ids if i not in msgs_mail_ids]
 remove_from_one_table(conn=conn,
                       sql_table='quarantine',
                       index_column='mail_id',
@@ -178,7 +208,7 @@ qr = conn.select('msgrcpt', what='rid')
 for i in qr:
     maddr_ids_in_use.add(i.rid)
 
-invalid_maddr_ids = [id for id in maddr_ids if id not in maddr_ids_in_use]
+invalid_maddr_ids = [i for i in maddr_ids if i not in maddr_ids_in_use]
 remove_from_one_table(conn=conn,
                       sql_table='maddr',
                       index_column='id',
@@ -210,7 +240,7 @@ except:
     # No outbound_wblist table
     pass
 
-invalid_mailaddr_ids = [id for id in mailaddr_ids if id not in wblist_ids]
+invalid_mailaddr_ids = [i for i in mailaddr_ids if i not in wblist_ids]
 remove_from_one_table(conn=conn,
                       sql_table='mailaddr',
                       index_column='id',
@@ -224,12 +254,11 @@ logger.info('`quarantine`: %-7.d' % (len(quar_mail_ids) - len(invalid_quar_mail_
 logger.info('     `maddr`: %-7.d' % (len(maddr_ids) - len(invalid_maddr_ids)))
 logger.info('  `mailaddr`: %-7.d' % (len(mailaddr_ids) - len(invalid_mailaddr_ids)))
 
-
 if counter_msgs \
-   or counter_msgrcpt \
-   or invalid_quar_mail_ids \
-   or invalid_maddr_ids \
-   or invalid_mailaddr_ids:
+        or counter_msgrcpt \
+        or invalid_quar_mail_ids \
+        or invalid_maddr_ids \
+        or invalid_mailaddr_ids:
     msg = 'Removed records: '
     msg += '%d in msgs, ' % counter_msgs
     msg += '%d in msgrcpt, ' % counter_msgrcpt
